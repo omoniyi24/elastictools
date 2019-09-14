984b5ad0046e8b9eaa5d80a0f434b1ad58480974
omoniyi24 <ilesanmi@Systemspecs.com.ng>
2019-09-14 23:49:33 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ with open('HISTORY.md') as history_file:
     HISTORY = history_file.read()
 
 setup_args = dict(
-    name='elastictools',
+    name='devsunkylogger',
     version='0.2.3',
     description='Useful tools to work with Elastic stack in Python',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     packages=find_packages(),
-    author='Thuc Nguyen',
-    author_email='gthuc.nguyen@gmail.com',
+    author='Ilesanmi Omoniyi',
+    author_email='omoniyi24@gmail.com',
     keywords=['Elastic', 'ElasticSearch', 'Elastic Stack', 'Python 3', 'Elastic 7'],
-    url='https://github.com/ncthuc/elastictools',
-    download_url='https://pypi.org/project/elastictools/'
+    url='https://github.com/omoniyi24/dev-sunky-logger'
 )
 
 install_requires = [
